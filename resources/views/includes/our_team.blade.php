14a0e12ee6f23d0f72cfdfcfe4d96958387dd5ab
FateevMaxim <fateev.maxim@gmail.com>
2022-02-07 22:02:32 +0600

    Changes

--- resources/views/includes/our_team.blade.php
+++ resources/views/includes/our_team.blade.php
@@ -15,17 +15,20 @@
         <div class="team-area-main">
             <div class="row">
                 <div class="col-md-12">
+
+
                     <div class="slider slider-for">
                         <!-- Service Slider Single -->
+                        @foreach($data as $actor)
                         <div class="single-item">
                             <div class="large-img">
-                                <img src="{{ asset('img/home-one/team/almassh.jpg') }}" alt="Алмас Шаяхмет">
+                                <img src="{{ asset('img/home-one/team/'.$actor->img) }}" alt="{{ $actor->name }}">
                             </div>
                             <div class="thumb-content text-right">
                                 <div class="thumb-personal-info">
                                     <div class="teamper-titel">
-                                        <h5>АЛМАС ШАЯХМЕТ</h5>
-                                        <span>Қабділман</span>
+                                        <h5>{{ $actor->name }}</h5>
+                                        <span>{{ $actor->role }}</span>
                                     </div>
                                     <div class="team-social">
                                         <ul>
@@ -33,151 +36,27 @@
                                             <li><a href="#"><i class="icofont icofont-instagram"></i></a></li>
                                         </ul>
                                     </div>
-                                    <p><b>Алмас Муратбекович Шаяхметов</b><br>
-                                       <i>Лауреат премии Союза Молодежи Казахстана «Серпер» (2013).</i>
-
-                                        1987 года рождения, уроженец с.Коктал Урджарского района Восточно-Казахстанской области.
+                                    <p><b>{{ $actor->name }}</b><br>
+                                        {{ $actor->short_desc }}
                                     </p>
                                 </div>
                             </div>
                         </div>
-                        <!-- Service Slider Single -->
-                        <!-- Service Slider Single -->
-                        <div class="single-item">
-                            <div class="large-img">
-                                <img src="{{ asset('img/home-one/team/asanm.jpg') }}" alt="Асан Мажит">
-                            </div>
-                            <div class="thumb-content text-right">
-                                <div class="thumb-personal-info">
-                                    <div class="teamper-titel">
-                                        <h5>АСАН МАЖИТ</h5>
-                                        <span>Ораз Жандосов</span>
-                                    </div>
-                                    <div class="team-social">
-                                        <ul>
-                                            <li><a href="#"><i class="icofont icofont-facebook"></i></a></li>
-                                            <li><a href="#"><i class="icofont icofont-instagram"></i></a></li>
-                                        </ul>
-                                    </div>
-                                    <p>Асан Мажит (Киркабаков) - актер. Окончил Казахскую Национальную академию искусств им. Т. Жургенова.
-                                        <br>
-                                        Семейное положение - женат.
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-                        <!-- Service Slider Single -->
-                        <!-- Service Slider Single -->
-                        <div class="single-item">
-                            <div class="large-img">
-                                <img src="{{ asset('img/home-one/team/igorg.jpg') }}" alt="">
-                            </div>
-                            <div class="thumb-content text-right">
-                                <div class="thumb-personal-info">
-                                    <div class="teamper-titel">
-                                        <h5>ГУЗУН ИГОРЬ ГЕОРГИЕВИЧ</h5>
-                                        <span>Иосиф Виссарионович Сталин</span>
-                                    </div>
-                                    <div class="team-social">
-                                        <ul>
-                                            <li><a href="#"><i class="icofont icofont-facebook"></i></a></li>
-                                            <li><a href="#"><i class="icofont icofont-instagram"></i></a></li>
-                                        </ul>
-                                    </div>
-                                    <p>Гузун Игорь Георгиевич (род. 30 мая 1960) — актёр, режиссёр, художник-постановщик, продюсер, член Гильдии Актёров России, член Союза кинематографистов России.</p>
-                                </div>
-                            </div>
-                        </div>
-                        <!-- Service Slider Single -->
-                        <!-- Service Slider Single -->
-                        <div class="single-item">
-                            <div class="large-img">
-                                <img src="{{ asset('img/home-one/team/darkhans.jpg') }}" alt="">
-                            </div>
-                            <div class="thumb-content text-right">
-                                <div class="thumb-personal-info">
-                                    <div class="teamper-titel">
-                                        <h5>ДАРХАН СУЛЕЙМЕНОВ</h5>
-                                        <span>Алгадай - сын Жамбыла</span>
-                                    </div>
-                                    <div class="team-social">
-                                        <ul>
-                                            <li><a href="#"><i class="icofont icofont-facebook"></i></a></li>
-                                            <li><a href="#"><i class="icofont icofont-instagram"></i></a></li>
-                                        </ul>
-                                    </div>
-                                    <p>Дархан Сулейменов - дата рождения 7 мая 1988</p>
-                                </div>
-                            </div>
-                        </div>
-                        <!-- Service Slider Single -->
-                        <!-- Service Slider Single -->
-                        <div class="single-item">
-                            <div class="large-img">
-                                <img src="{{ asset('img/home-one/team/doszhanzh.jpg') }}" alt="">
-                            </div>
-                            <div class="thumb-content text-right">
-                                <div class="thumb-personal-info">
-                                    <div class="teamper-titel">
-                                        <h5>ЖАНБОТАЕВ ДОСЖАН </h5>
-                                        <span>Жамбыл Жабаев</span>
-                                    </div>
-                                    <div class="team-social">
-                                        <ul>
-                                            <li><a href="#"><i class="icofont icofont-facebook"></i></a></li>
-                                            <li><a href="#"><i class="icofont icofont-instagram"></i></a></li>
-                                        </ul>
-                                    </div>
-                                    <p>Жанботаев, Досжан Абдралимович - актёр, театральный режиссёр, театральный педагог</p>
-                                </div>
-                            </div>
-                        </div>
-                        <!-- Service Slider Single -->
-                        <!-- Service Slider Single -->
-                        <div class="single-item">
-                            <div class="large-img">
-                                <img src="{{ asset('img/home-one/team/farkhatm.jpg') }}" alt="">
-                            </div>
-                            <div class="thumb-content text-right">
-                                <div class="thumb-personal-info">
-                                    <div class="teamper-titel">
-                                        <h5>ФАРХАД МАХМУДОВ</h5>
-                                        <span>Мирзоян</span>
-                                    </div>
-                                    <div class="team-social">
-                                        <ul>
-                                            <li><a href="#"><i class="icofont icofont-facebook"></i></a></li>
-                                            <li><a href="#"><i class="icofont icofont-instagram"></i></a></li>
-                                        </ul>
-                                    </div>
-                                    <p>Фарха́д Бахады́рович Махму́дов — российский актёр театра и кино</p>
-                                </div>
-                            </div>
-                        </div>
+                        @endforeach
                         <!-- Service Slider Single -->
                     </div>
+
+
                     <!-- Service Thumb Area Start -->
                     <div class="slider slider-nav">
+                        @foreach($data as $thumb)
                         <div class="thumb-single">
-                            <img src="{{ asset('img/home-one/team/almassh110.jpg') }}" alt="">
+                            <img src="{{ asset('img/home-one/team/'.$thumb->small_img) }}" alt="{{ $thumb->name }}">
                         </div>
-                        <div class="thumb-single">
-                            <img src="{{ asset('img/home-one/team/asanm110.jpg') }}" alt="">
-                        </div>
-                        <div class="thumb-single">
-                            <img src="{{ asset('img/home-one/team/igorg110.jpg') }}" alt="">
-                        </div>
-                        <div class="thumb-single">
-                            <img src="{{ asset('img/home-one/team/darkhans110.jpg') }}" alt="">
-                        </div>
-                        <div class="thumb-single">
-                            <img src="{{ asset('img/home-one/team/doszhanzh110.jpg') }}" alt="">
-                        </div>
-                        <div class="thumb-single">
-                            <img src="{{ asset('img/home-one/team/farkhatm110.jpg') }}" alt="">
-                        </div>
+                        @endforeach
                     </div>
                     <!-- Service Thumb Area Start -->
+
                 </div>
             </div>
             <!-- Service Main Area End -->
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,9 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('includes.main');
-});
-Route::get('/about', function () {
-    return view('includes.about');
-});
+Route::get('/', [Controllers\MainController::class, 'index']);
