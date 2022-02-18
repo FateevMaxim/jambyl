281d903667876379a8a8b1ed08970564935d871e
FateevMaxim <fateev.maxim@gmail.com>
2022-02-18 23:20:32 +0600

    New image gallery

--- resources/views/includes/gallery.blade.php
+++ resources/views/includes/gallery.blade.php
@@ -4,7 +4,7 @@
             <!-- Section Titel -->
             <div class="col-md-12">
                 <div class="section-titel style-3 text-left">
-                    <h2>КАДРЫ СО <span>СЪЁМОК</span></h2>
+                    <h2>КАДРЫ СО СЪЕМОЧНОЙ <span>ПЛОЩАДКИ</span></h2>
                     <p>Интересные кадры с процесса съёмок и монтажа </p>
                 </div>
             </div>
@@ -17,62 +17,110 @@
             <!-- Latest Shot/Portfolio Single -->
             <div class="gallery-single">
                 <div class="gallery-image">
-                    <img src="{{ asset('img/gallery/1_1.jpg') }}" alt="">
+                    <img src="{{ asset('img/gallery/1_1.jpg') }}" alt="Кадры со съёмок фильма Жамбыл">
                     <a href="{{ asset('img/gallery/1.jpg') }}" class="popup-gallery"><i class="icofont-search"></i></a>
                 </div>
             </div>
             <div class="gallery-single">
                 <div class="gallery-image">
-                    <img src="{{ asset('img/gallery/2_2.jpg') }}" alt="">
+                    <img src="{{ asset('img/gallery/2_2.jpg') }}" alt="Кадры со съёмок фильма Жамбыл">
                     <a href="{{ asset('img/gallery/2.jpg') }}" class="popup-gallery"><i class="icofont-search"></i></a>
                 </div>
             </div>
             <div class="gallery-single">
                 <div class="gallery-image">
-                    <img src="{{ asset('img/gallery/3_3.jpg') }}" alt="">
+                    <img src="{{ asset('img/gallery/3_3.jpg') }}" alt="Кадры со съёмок фильма Жамбыл">
                     <a href="{{ asset('img/gallery/3.jpg') }}" class="popup-gallery"><i class="icofont-search"></i></a>
                 </div>
             </div>
             <div class="gallery-single">
                 <div class="gallery-image">
-                    <img src="{{ asset('img/gallery/4_4.jpg') }}" alt="">
+                    <img src="{{ asset('img/gallery/4_4.jpg') }}" alt="Кадры со съёмок фильма Жамбыл">
                     <a href="{{ asset('img/gallery/4.jpg') }}" class="popup-gallery"><i class="icofont-search"></i></a>
                 </div>
             </div>
             <div class="gallery-single">
                 <div class="gallery-image">
-                    <img src="{{ asset('img/gallery/5_5.jpg') }}" alt="">
+                    <img src="{{ asset('img/gallery/5_5.jpg') }}" alt="Кадры со съёмок фильма Жамбыл">
                     <a href="{{ asset('img/gallery/5.jpg') }}" class="popup-gallery"><i class="icofont-search"></i></a>
                 </div>
             </div>
             <div class="gallery-single">
                 <div class="gallery-image">
-                    <img src="{{ asset('img/gallery/6_6.jpg') }}" alt="">
+                    <img src="{{ asset('img/gallery/6_6.jpg') }}" alt="Кадры со съёмок фильма Жамбыл">
                     <a href="{{ asset('img/gallery/6.jpg') }}" class="popup-gallery"><i class="icofont-search"></i></a>
                 </div>
             </div>
             <div class="gallery-single">
                 <div class="gallery-image">
-                    <img src="{{ asset('img/gallery/7_7.jpg') }}" alt="">
+                    <img src="{{ asset('img/gallery/7_7.jpg') }}" alt="Кадры со съёмок фильма Жамбыл">
                     <a href="{{ asset('img/gallery/7.jpg') }}" class="popup-gallery"><i class="icofont-search"></i></a>
                 </div>
             </div>
             <div class="gallery-single">
                 <div class="gallery-image">
-                    <img src="{{ asset('img/gallery/8_8.jpg') }}" alt="">
+                    <img src="{{ asset('img/gallery/8_8.jpg') }}" alt="Кадры со съёмок фильма Жамбыл">
                     <a href="{{ asset('img/gallery/8.jpg') }}" class="popup-gallery"><i class="icofont-search"></i></a>
                 </div>
             </div>
             <div class="gallery-single">
                 <div class="gallery-image">
-                    <img src="{{ asset('img/gallery/9_9.jpg') }}" alt="">
+                    <img src="{{ asset('img/gallery/9_9.jpg') }}" alt="Кадры со съёмок фильма Жамбыл">
                     <a href="{{ asset('img/gallery/9.jpg') }}" class="popup-gallery"><i class="icofont-search"></i></a>
                 </div>
             </div>
             <div class="gallery-single">
                 <div class="gallery-image">
-                    <img src="{{ asset('img/gallery/10_10.jpg') }}" alt="">
+                    <img src="{{ asset('img/gallery/10_10.jpg') }}" alt="Кадры со съёмок фильма Жамбыл">
                     <a href="{{ asset('img/gallery/10.jpg') }}" class="popup-gallery"><i class="icofont-search"></i></a>
+                </div>
+            </div>
+            <div class="gallery-single">
+                <div class="gallery-image">
+                    <img src="{{ asset('img/gallery/11_11.jpg') }}" alt="Кадры со съёмок фильма Жамбыл">
+                    <a href="{{ asset('img/gallery/11.jpg') }}" class="popup-gallery"><i class="icofont-search"></i></a>
+                </div>
+            </div>
+            <div class="gallery-single">
+                <div class="gallery-image">
+                    <img src="{{ asset('img/gallery/12_12.jpg') }}" alt="Кадры со съёмок фильма Жамбыл">
+                    <a href="{{ asset('img/gallery/12.jpg') }}" class="popup-gallery"><i class="icofont-search"></i></a>
+                </div>
+            </div>
+            <div class="gallery-single">
+                <div class="gallery-image">
+                    <img src="{{ asset('img/gallery/13_13.jpg') }}" alt="Кадры со съёмок фильма Жамбыл">
+                    <a href="{{ asset('img/gallery/13.jpg') }}" class="popup-gallery"><i class="icofont-search"></i></a>
+                </div>
+            </div>
+            <div class="gallery-single">
+                <div class="gallery-image">
+                    <img src="{{ asset('img/gallery/14_14.jpg') }}" alt="Кадры со съёмок фильма Жамбыл">
+                    <a href="{{ asset('img/gallery/14.jpg') }}" class="popup-gallery"><i class="icofont-search"></i></a>
+                </div>
+            </div>
+            <div class="gallery-single">
+                <div class="gallery-image">
+                    <img src="{{ asset('img/gallery/15_15.jpg') }}" alt="Кадры со съёмок фильма Жамбыл">
+                    <a href="{{ asset('img/gallery/15.jpg') }}" class="popup-gallery"><i class="icofont-search"></i></a>
+                </div>
+            </div>
+            <div class="gallery-single">
+                <div class="gallery-image">
+                    <img src="{{ asset('img/gallery/16_16.jpg') }}" alt="Кадры со съёмок фильма Жамбыл">
+                    <a href="{{ asset('img/gallery/16.jpg') }}" class="popup-gallery"><i class="icofont-search"></i></a>
+                </div>
+            </div>
+            <div class="gallery-single">
+                <div class="gallery-image">
+                    <img src="{{ asset('img/gallery/17_17.jpg') }}" alt="Кадры со съёмок фильма Жамбыл">
+                    <a href="{{ asset('img/gallery/17.jpg') }}" class="popup-gallery"><i class="icofont-search"></i></a>
+                </div>
+            </div>
+            <div class="gallery-single">
+                <div class="gallery-image">
+                    <img src="{{ asset('img/gallery/18_18.jpg') }}" alt="Кадры со съёмок фильма Жамбыл">
+                    <a href="{{ asset('img/gallery/18.jpg') }}" class="popup-gallery"><i class="icofont-search"></i></a>
                 </div>
             </div>
             <!-- Latest Shot/Portfolio Single -->
